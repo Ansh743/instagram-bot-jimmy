f6bdba271145f8d510a64925d20411365a1b4718
Anshul M <anshmaske743@gmail.com>
2021-01-07 00:28:00 +0530

    The bot now can like top 9 posts of a certain tag
    To like posts of a certain tag/s in the driver.py file update the tags and after list.
    The 'after' list is used as a post-fix to each tag in 'tags' list.

--- driver.py
+++ driver.py
@@ -2,13 +2,12 @@
 from selenium import webdriver
 from time import sleep
 from selenium.webdriver.common.keys import Keys
-from random import randint
 from credentials import id,pswd
 
 class Bot(): 
     def __init__(self):
         self.login(id)
-    
+        
     def login(self, id):
         self.driver = webdriver.Chrome(r'.\chromedriver.exe')   
         self.driver.get('https://www.instagram.com/')
@@ -29,10 +28,47 @@ class Bot():
         not_now_btn = self.driver.find_element_by_xpath('/html/body/div[4]/div/div/div/div[3]/button[2]')
         not_now_btn.click()
     
-    def srch_tags():
-        None
+    def srch_tags(self, tags, after):
+        allTags = list()
+        for each in tags:
+            for every in after:
+                allTags.append(each+every)
+                
+        search_bar = self.driver.find_element_by_xpath('//*[@id="react-root"]/section/nav/div[2]/div/div/div[2]/div/div')
+        search_bar.click()
+        sleep(0.5)
+        search_bar = self.driver.find_element_by_xpath('//*[@id="react-root"]/section/nav/div[2]/div/div/div[2]/input')
+        search_bar.click()
+        
+        for each in allTags:
+            search_bar.send_keys('#'+each)
+            sleep(3)
+            tag = self.driver.find_element_by_xpath('//*[@id="react-root"]/section/nav/div[2]/div/div/div[2]/div[4]/div/a[1]')
+            tag.click()
+            sleep(3)
+            for x in range(1,4):
+                for y in range(1,4):
+                    currentPost = self.driver.find_element_by_xpath('//*[@id="react-root"]/section/main/article/div[1]/div/div/div['+str(x)+']/div['+str(y)+']/a/div')
+
+                    currentPost.click()
+                    sleep(2)
+                    
+                    like_button = self.driver.find_element_by_xpath('/html/body/div[5]/div[2]/div/article/div[3]/section[1]/span[1]/button')
+                    like_button.click()
+                    
+                    sleep(0.5)
+                    close = self.driver.find_element_by_xpath('/html/body/div[5]/div[3]/button')
+                    close.click()
+        
+
+        
         
 def main():
     jimmy = Bot()
+    tags = ['car', 'bike', 'plane', 'helicopter']
+    after = ['love', 'lover']
+    jimmy.srch_tags(tags, after)
+    
+    
 if __name__ == '__main__':
     main()
